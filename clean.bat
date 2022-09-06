3b6e8ffedc56038b40cf3bc3d8124122a7799d0c
LYM\luritech <kevin02678@gmail.com>
2022-09-06 14:26:50 +0900

    no message

---
+++ clean.bat
@@ -0,0 +1,2 @@
+del /s *.sdf;*.ipch;*.obj;*.exp;*.log;*.ilk;*.pch;*.pdb;*.idb;*.tlog;*.lastbuildstate;*.tlh;*.tli;*.aps
+del /s /a:h *.sdf;*.ipch;*.obj;*.exp;*.log;*.ilk;*.pch;*.pdb;*.idb;*.tlog;*.lastbuildstate;*.tlh;*.tli;*.aps
